abf7be8b81f107a9920c602d61af5b76f241459d
QuangDo <do.quang03@gmail.com>
2025-02-17 14:53:05 +0700

    chore: Configure Docker and Vite for host binding

--- Dockerfile
+++ Dockerfile
@@ -22,4 +22,4 @@ RUN npm run build
 EXPOSE 3000
 
 # Lệnh chạy ứng dụng
-CMD ["npm", "run", "preview"]
+CMD ["npm", "run", "preview", "--", "--host", "0.0.0.0"]
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Đặt alias '@' trỏ đến thư mục 'src'
