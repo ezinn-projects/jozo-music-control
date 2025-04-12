beb8879a3c196d8f3f9d2cf4e01bf4c7a9d0f603
QuangDoo <do.quang039@gmail.com>
2025-04-12 22:52:39 +0700

    ,

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -26,26 +26,6 @@ const Modal: React.FC<ModalProps> = ({
         </p>
         <div className="flex items-center justify-center space-x-4">
           <button
-            onClick={onAddToTop}
-            className="bg-lightpink text-gray-900 py-2 w-full px-4 rounded-lg hover:bg-opacity-80 flex flex-col items-center gap-y-2"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
-              />
-            </svg>
-            Thêm vào đầu danh sách
-          </button>
-          <button
             onClick={onAddToEnd}
             className="bg-lightpink text-gray-900 w-full py-2 px-4 rounded-lg hover:bg-opacity-80 flex flex-col items-center gap-y-2"
           >
@@ -64,6 +44,27 @@ const Modal: React.FC<ModalProps> = ({
               />
             </svg>
             Thêm vào cuối danh sách
+          </button>
+
+          <button
+            onClick={onAddToTop}
+            className="bg-lightpink text-gray-900 py-2 w-full px-4 rounded-lg hover:bg-opacity-80 flex flex-col items-center gap-y-2"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+              />
+            </svg>
+            Thêm vào đầu danh sách
           </button>
         </div>
 
