e2b470e52c54548b5b20afcd5455e0e73d3f6ffd
QuangDoo <do.quang039@gmail.com>
2025-04-15 20:43:39 +0700

    feat(header): Add notification functionality and integrate useRoom hook for sending messages

--- src/routes/ProtectedRoute.tsx
+++ src/routes/ProtectedRoute.tsx
@@ -1,14 +1,15 @@
-import { useTimer } from "@/contexts/TimerContext";
+// import { useTimer } from "@/contexts/TimerContext";
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+// import { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute: React.FC = () => {
-  const { timeLeft } = useTimer();
+  // const { timeLeft } = useTimer();
 
   // Nếu hết giờ, chuyển hướng sang trang QR Code
-  if (timeLeft <= 0) {
-    return <Navigate to="/qr-code" replace />;
-  }
+  // if (timeLeft <= 0) {
+  //   return <Navigate to="/qr-code" replace />;
+  // }
 
   // Nếu còn thời gian, render các route bên trong
   return <Outlet />;
